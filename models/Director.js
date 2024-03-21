b6051956874b4f62bf6b7651861d73730257bb1c
taner <tanerozer16@gmail.com>
2024-03-21 16:57:35 +0300

    DirectorSchema added

---
+++ models/Director.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DirectorSchema = new Schema({
+  name: {
+    type: String,
+    maxlength: 60,
+    minlength: 2,
+  },
+  surname: {
+    type: String,
+    maxlength: 60,
+    minlength: 2,
+  },
+  bio: {
+    type: String,
+    maxlenght: 1000,
+    minlength: 60,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("director", DirectorSchema);
